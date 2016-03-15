d40341404f33ab749a743c7b8d05b80c70d0e55b
Sherlock-szx <sherlockatszx@qq.com>
2016-03-15 23:59:28 +0800

    service debug and add template
    service reload automatically, and add template of html file

--- qiubai/serviceFlask.py
+++ qiubai/serviceFlask.py
@@ -6,11 +6,15 @@ Created on Tue Mar 15 23:25:44 2016
 """
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #return "Hello World!This is a Flask webpage"
+    return render_template("qiubai_index.html")
+    
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
